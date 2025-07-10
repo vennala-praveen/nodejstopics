c420495df0546ccaef2d264cd3c54b41cda4fc8a
Praveen Vennala <praveen98vennala@gmail.com>
2025-07-10 11:06:20 +0530

    mailer file added

--- EmailUseNodeJS/email.js
+++ EmailUseNodeJS/email.js
@@ -1,3 +1,40 @@
 var nm = require('nodemailer');
 
-var tr
+var transporter = nm.createTransport(
+    {
+        host:"smtp.gmail.com",
+        port:587,
+        secure: false,
+        auth:{
+            user:'praveen98vennala@gmail.com',
+            pass:'mtekqqxzjkhfarqy'
+        }
+    }
+);
+
+var options = {
+    from: 'praveen98vennala@gmail.com',
+    to:'praveen98vennala@gmail.com',
+    subject:"Testing node emails",
+    html:`<h1>Hello welcome to our application</h1>
+        <img src='cid:food' width='200px'>
+        <button>Click Here </button>
+    `,
+    attachments:[
+        {
+            filename:'food.jpeg',
+            path:"https://images.pexels.com/photos/421809/pexels-photo-421809.jpeg",
+            cid:'food'
+        }
+    ]
+}
+
+transporter.sendMail(
+    options, function(error,info){
+        if(error){
+            console.log("Error:", error)
+        }else{
+            console.log("Email sent:", info.response)
+        }
+    }
+)
